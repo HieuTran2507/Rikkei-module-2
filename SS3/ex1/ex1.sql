e93667c0d0726779d69b5f61deb020f0ff6d75fd
hieutran <trantuanhieu2507@gmail.com>
2026-01-20 13:12:27 +0900

    ss3

--- SS3/ex1/ex1.sql
+++ SS3/ex1/ex1.sql
@@ -1,15 +1,15 @@
 create schema session3;
 use session3;
 create table ex1_classes(
-	classID int primary key not null,
+	classID int primary key,
     className varchar(5) not null,
     startYear year,
     endYear year,
-    check (endYear > startYear)
+    check (endYear >= startYear)
 );
 
 create table ex1_students(
-	studentID int primary key not null,
+	studentID int primary key,
     classID int not null,
     studentName varchar(100) not null,
     birthday date,
